b4418e27459d0f9a1bac59f961239a1c34512ede
EdaYanaz <155250514+EdaYanaz@users.noreply.github.com>
2024-04-08 00:09:28 +0300

    Proje

---
+++ Assets/Scripts/Cikk.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cikk : MonoBehaviour
+{
+    public void Cik()
+    {
+        Application.Quit();
+    }
+}
